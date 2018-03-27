487814314521f684d01da832c52fb854c963af8f
amaliukh <37339447+amaliukh@users.noreply.github.com>
2018-03-27 14:00:53 +0300

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,27 @@
+package com.tasks3.fibonacci;
+
+
+
+public class Fibonacci {
+
+	
+
+	public long getNumber(int position) {
+
+		if (position < 1) {
+
+			return -1L;
+
+		}
+
+		else if (position == 1 || position == 2) {
+
+			return 1L;
+
+		}
+
+		else return getNumber(position-1)+getNumber(position-2);
+
+	}
+
+}
---
+++ LinkedList.java
@@ -0,0 +1,129 @@
+package com.tasks3.linkedlist;
+
+
+
+public class LinkedList {
+
+    
+
+    private Node start;
+
+    private Node end;
+
+    private int size = 0;
+
+
+
+    public LinkedList() {
+
+
+
+    }
+
+
+
+    public void add(Integer data) {
+
+        Node newNode = new Node();
+
+        newNode.setData(data);
+
+
+
+        if (size == 0)
+
+            start = newNode;
+
+        else
+
+            end.setNext(newNode);
+
+
+
+        end = newNode;
+
+        size++;
+
+    }
+
+
+
+    public Integer get(int index) {
+
+        return findNodeByIndex(index).getData();
+
+    }
+
+
+
+    public boolean delete(int index) {
+
+        if (findNodeByIndex(index) != null ) {
+
+
+
+            if (index != 0)
+
+                findNodeByIndex(index - 1).setNext(findNodeByIndex(index+1));
+
+            else
+
+                start = start.getNext();
+
+
+
+            size--;
+
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+
+
+    public int size() {
+
+        return size;
+
+    }
+
+
+
+    private Node findNodeByIndex(int index) {
+
+        if (index < size && index >= 0) {
+
+            Node curNode = start;
+
+            int curIndex = 0;
+
+            while (curIndex < index){
+
+                if (curNode.getNext() != null) {
+
+                    curNode = curNode.getNext();
+
+                    curIndex++;
+
+                }
+
+
+
+            }
+
+            return curNode;
+
+        }
+
+        else
+
+            return null;
+
+    }
+
+
+
+}
