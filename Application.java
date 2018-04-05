06bd7511e049b47863f3b960d6c55a39e634c034
amaliukh <37339447+amaliukh@users.noreply.github.com>
2018-04-05 10:31:49 +0300

    Add files via upload

--- Application.java
+++ Application.java
@@ -1,31 +1,79 @@
-package com.tasks5.command;
+package com.tasks6.rle_decoder;
 
-public class Application {
-	public static void main(String[] args) {
-		if (args != null ){
-            int length = args.length;
-            if  (length == 1) {
-                String first = args[0];
-                if ("help".equals(first))
-                    new Help().execute();
-                else if ("exit".equals(first))
-                    new Exit().execute();
-                else
-                    System.out.println("Error");
+ 
+
+public class Application{
+
+    public static void main( String[] args )
+
+    {
+
+        String input = args[0];
+
+        String output = "";
+
+        if((input != "") && ((input.charAt(0) < '0') || ( input.charAt(0) > '9'))) {
+
+            for (int i = 0; i < input.length(); i++) {
+
+
+
+               // char k = input.charAt(i);
+
+
+
+                if (i < input.length() - 1) {
+
+                    if (input.charAt(i + 1) == input.charAt(i)) {
+
+                        output = "";
+
+                        break;
+
+                    }
+
+                }
+
+
+
+                if (((input.charAt(i) >= '1') && (input.charAt(i) <= '9'))) {
+
+
+
+                    if (i < input.length() - 1) {
+
+                        if ((input.charAt(i + 1) >= '0') && ( input.charAt(i + 1) <= '9')) {
+
+                            output = "";
+
+                            break;
+
+                        }
+
+                    }
+
+
+
+                    for (int j = 0; j < Character.getNumericValue(input.charAt(i)) - 1; j++) {
+
+                        output += input.charAt(i - 1);
+
+                    }
+
+
+
+                }
+
+                if ((input.charAt(i) < '0') || ( input.charAt(i) > '9'))
+
+                    output += input.charAt(i);
+
             }
-            else if( length ==2 ) {
-                String first = args[0];
-                if ("echo".equals(first))
-                    new Echo(args).execute();
-                else if ("date".equals(first) && "now".endsWith(args[1]))
-                    new Date().execute();
-                else
-                    System.out.println("Error");
-            }
-            else
-                System.out.println("Error");
+
         }
-        else
-            System.out.println("Error");
+
+        System.out.println(output);
+
     }
+
 }
